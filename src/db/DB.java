607bfd79a53db0f812518c0701f3c94095904079
jonathan <jonathanslisboa@gmail.com>
2022-02-18 00:31:33 -0300

    findById implementation

--- src/db/DB.java
+++ src/db/DB.java
@@ -4,7 +4,9 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
@@ -25,22 +27,44 @@ public class DB {
 	}
 
 	public static void closeConnection() {
-		if(conn != null) {
+		if (conn != null) {
 			try {
-			conn.close();
-			}
-			catch (SQLException e) {
+				conn.close();
+			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
 	}
+
 	private static Properties loadProperties() {
 		try (FileInputStream fs = new FileInputStream("db.properties")) {
 			Properties props = new Properties();
 			props.load(fs);
 			return props;
-		} catch (IOException e) {
+		} 
+		catch (IOException e) {
 			throw new DbException(e.getMessage());
+		}
+	}
+	public static void closeStatement(Statement st) {
+		if (st != null) {
+			try {
+				st.close();
+			} 
+			catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} 
+			catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
 		}
 	}
 }
